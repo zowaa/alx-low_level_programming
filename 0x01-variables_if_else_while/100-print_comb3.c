e77e4b87ce3fa395b9fe3a17d7f35195f34e01a2
zowaa <elfilfatimaezzahra0@gmail.com>
2024-08-16 09:42:57 -0700

    task 100 , C variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i,j;
+
+	i = 0;
+	while (i <= 8)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (!(i == 8 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;	
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
