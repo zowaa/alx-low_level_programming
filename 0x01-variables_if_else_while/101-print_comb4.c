673e289ccb7a5ab1a497098171b07154bfc930da
zowaa <elfilfatimaezzahra0@gmail.com>
2024-08-16 09:57:40 -0700

    task 101, C variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
