ed038f10dd731606587697d4e8626ee308114fdf
zowaa <elfilfatimaezzahra0@gmail.com>
2024-09-10 12:43:34 -0700

    task 5, Even more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: String to search in
+ * @needle: Substring to find
+ *
+ * Return: First occurence of the substring , or NULL if none found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int	i, j, n_l;
+	char	*p;
+
+	i = 0;
+	p = NULL;
+	n_l = 0;
+	while (needle[n_l])
+		n_l++;
+	while (haystack[i])
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			while (needle[j])
+			{
+				if (needle[j] == haystack[i + j])
+					j++;
+				else
+					break;
+			}
+			if (j == n_l)
+			{
+				p = &haystack[i];
+				break;
+			}
+		}
+		i++;
+	}
+	return (p);
+}
