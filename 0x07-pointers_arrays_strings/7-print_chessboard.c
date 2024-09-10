4d236d3ce2a26ef731fd38c908d64f23247334b7
zowaa <elfilfatimaezzahra0@gmail.com>
2024-09-10 16:02:39 -0700

    task 7, Even more pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: Pointer to an array of 8 chars
+ *
+ * Return: Void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int	i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
